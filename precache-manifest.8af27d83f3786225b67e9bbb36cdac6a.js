a8c4efe6d6a9c2ddd591e158cfa8c3e874553626
codetologist <codetologist@gmail.com>
2020-03-22 12:58:45 -0400

    Updates

---
+++ precache-manifest.8af27d83f3786225b67e9bbb36cdac6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0bfdd0c5612124a1b1c478c8994033",
+    "url": "/radioaficionpr/index.html"
+  },
+  {
+    "revision": "eb3ff0bb78018de07863",
+    "url": "/radioaficionpr/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bee1725973367231b994",
+    "url": "/radioaficionpr/static/js/2.786205b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/radioaficionpr/static/js/2.786205b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb3ff0bb78018de07863",
+    "url": "/radioaficionpr/static/js/main.dc76f7d6.chunk.js"
+  },
+  {
+    "revision": "58080b3d98d5935fab8a",
+    "url": "/radioaficionpr/static/js/runtime-main.8fb2093a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/radioaficionpr/static/media/logo.5d5d9eef.svg"
+  }
+]);
